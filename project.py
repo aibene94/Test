41a611faedbabda84548322f9834dd4b5855d155
Benedikt Aigner <aigner.benedikt@gmx.de>
2020-08-04 21:59:17 +0200

    Condition for taking or rejection a point

--- project.py
+++ project.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 1000
+N = 10000
+r = 0
 
 points = []
+
 circle = lambda x : np.sqrt(1 - x*x)
 x = np.linspace(0,1,101)
 y = circle(x)
@@ -15,10 +17,11 @@ for k in range(N):
 
 
     p = np.array([i, j])
-
-
     points.append(p)
+    if i*i + j*j < 1:
+        r = r + 1
 
+print("pi is roughtly", (r/N)*4)
 
 points = np.transpose(points)
 plt.plot(x,y,'b--', linewidth=3.0)
